77f61daa224981c65a3c10ad11aef57c2b921a10
milladka <mihancivil@gmail.com>
2018-11-04 14:53:38 +0330

    suuheader and box login view ok

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 <h1>WebSite YektaFelez</h1>
+<p>Laravel base v 5.6.0</p>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
         <link href="{{ asset('css/bootstrap-grid.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/bootstrap-reboot.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/bootstrap-rtl.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/flickity.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
         <title>تست</title>
     </head>
@@ -54,18 +55,77 @@
                     </div>
                 </nav>
     </header>
+    {{--sub header section--}}
+    <main role="main">
+        <section class="subheader rtl">
+            <div>
+                <div>
+                     <div class="content"></div>
+                    <div class="text-item">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="title-section">
+                                    <h1>یکتا فلز خاورمیانه پیشرو در صنعت</h1>
+                                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+                                    <a href="">درباره یکتافلز</a>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="section-from">
+                                    <div class="title-form">ورود به پنل کاربری</div>
+                                    <p>جهت ورود به پنل کاربری نام کاربری و رمز عبور را وارد کنید</p>
+                                    <form action="">
+                                        <div class="form-group row ">
+                                            <label for="inputEmail3" class="offset-md-1 col-sm-3 col-form-label">نام کاربری</label>
+                                            <div class="col-sm-6">
+                                                <input type="email" class="form-control" id="inputEmail3">
+                                            </div>
+                                        </div>
+                                        <br>
+                                        <div class="form-group row ">
+                                            <label for="inputEmail3" class="offset-md-1 col-sm-3 col-form-label">کلمه عبور</label>
+                                            <div class="col-sm-6">
+                                                <input type="password" class="form-control" id="inputEmail3">
+                                            </div>
+                                        </div>
+                                        <br>
+                                        <div class="form-group row">
+                                            <div class="col-sm-10 btn-submit">
+                                                <button type="submit" class="btn btn-warning">ورود</button>
+                                            </div>
+                                        </div>
+                                        <br>
+                                        <div class="row">
+                                            <div class="col-6 text-center"><a class="forget-pass-link" href="">فراموشی رمز عبور</a></div>
+                                            <div class="col-6"><a class="new-user-link" href="">ثبت نام کاربر جدید</a></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+            </div>
+            <div class="subheader-bg"></div>
+        </section>
+        <section class="product rtl">
+            <div class="linerup"></div>
+            <h2>محصولات</h2>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+        </section>
+    </main>
 
 
 
 
 
 
-
-     <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
      <script type="text/javascript" src="{{ asset('js/jquery.slim.min.js') }}"></script>
      <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
      <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
      <script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
+     <script type="text/javascript" src="{{ asset('js/flickity.pkgd.min.js') }}"></script>
      <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
     </body>
 </html>
